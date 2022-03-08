3d8180988b305694ac7d9f947bbbd043dcae4182
AKosmachyov <a.kosmachyov@gmail.com>
2022-03-08 18:47:06 +0100

    add empty project

---
+++ iOS-UIKit-elements/iOS-UIKit-elements/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-UIKit-elements
+//
+//  Created by Alexander Kosmachyov on 8.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
